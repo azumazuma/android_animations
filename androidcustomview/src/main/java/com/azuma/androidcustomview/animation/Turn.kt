2a92a645aa3238b515da55f0cbec08b0d18c94a3
Ryuse Azuma <azuma.ryusei@ditgroup.jp>
2019-11-12 12:55:37 +0900

    add anime

--- androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Turn.kt
+++ androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Turn.kt
@@ -6,6 +6,30 @@ import android.view.View
 import com.azuma.androidcustomview.util.AnimSet
 
 class Turn {
+    fun inSide(view: View, duration: Long = 1000): AnimatorSet {
+        val x = view.width.toFloat() / 2
+        val y = view.height.toFloat() / 2
+
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", -720f, 0f)
+        val o2: ObjectAnimator = ObjectAnimator.ofFloat(view, "pivotX", x, x)
+        val o3: ObjectAnimator = ObjectAnimator.ofFloat(view, "pivotY", y, y)
+        val o4: ObjectAnimator = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f)
+
+        return AnimSet.set(view, duration, o1, o2, o3, o4)
+    }
+
+    fun outSide(view: View, duration: Long = 1000): AnimatorSet {
+        val x = view.width.toFloat() / 2
+        val y = view.height.toFloat() / 2
+
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", 0f, 720f)
+        val o2: ObjectAnimator = ObjectAnimator.ofFloat(view, "pivotX", x, x)
+        val o3: ObjectAnimator = ObjectAnimator.ofFloat(view, "pivotY", y, y)
+        val o4: ObjectAnimator = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f)
+
+        return AnimSet.set(view, duration, o1, o2, o3, o4)
+    }
+
     fun inSideAxisTopLeft(view: View, duration: Long = 1000): AnimatorSet {
         val x = view.paddingLeft.toFloat()
         val y = 0f
--- app/src/main/java/com/customview/azuma/customviewapp/MainActivity.kt
+++ app/src/main/java/com/customview/azuma/customviewapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         // https://github.com/gayanvoice/android-view-animations-daimajia-kotlin
         val box = findViewById<View>(R.id.box)
         box.setOnClickListener {
-            Snap().inSideY(box).start()
+            Turn().outSide(box).start()
         }
     }
 }
