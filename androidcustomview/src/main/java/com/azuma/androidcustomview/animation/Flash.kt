ddc22598978f07066503c2215a36088aae10ed01
Ryuse Azuma <azuma.ryusei@ditgroup.jp>
2019-11-13 12:57:15 +0900

    add anim

---
+++ androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Flash.kt
@@ -0,0 +1,26 @@
+package com.azuma.androidcustomview.animation
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.view.View
+import com.azuma.androidcustomview.util.AnimSet
+
+class Flash {
+    fun week(view: View, duration: Long = 1000): AnimatorSet {
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f, 0f, 1f)
+
+        return AnimSet.set(view, duration, o1)
+    }
+
+    fun medium(view: View, duration: Long = 1000): AnimatorSet {
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f, 0f, 1f, 0f, 1f)
+
+        return AnimSet.set(view, duration, o1)
+    }
+
+    fun strength(view: View, duration: Long = 1000): AnimatorSet {
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f, 0f, 1f, 0f, 1f, 0f, 1f)
+
+        return AnimSet.set(view, duration, o1)
+    }
+}
--- androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Shake.kt
+++ androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Shake.kt
@@ -10,29 +10,29 @@ class Shake {
         val width = (view.width).toFloat()
         val power = (width / 300.0).toFloat()
 
-        val object1: ObjectAnimator = ObjectAnimator.ofFloat(view, "translationX", 0f * power, -25f * power, 20f * power, -15f * power, 10f * power, -5f * power, 0f * power, 0f)
-        val object2: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", 0f, -5f, 3f, -3f, 2f, -1f, 0f)
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "translationX", 0f * power, -25f * power, 20f * power, -15f * power, 10f * power, -5f * power, 0f * power, 0f)
+        val o2: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", 0f, -5f, 3f, -3f, 2f, -1f, 0f)
 
-        return AnimSet.set(view, duration, object1, object2)
+        return AnimSet.set(view, duration, o1, o2)
     }
 
     fun medium(view: View, duration: Long = 1000): AnimatorSet {
         val width = (view.width).toFloat()
         val power = (width / 200.0).toFloat()
 
-        val object1: ObjectAnimator = ObjectAnimator.ofFloat(view, "translationX", 0f * power, -25f * power, 20f * power, -15f * power, 10f * power, -5f * power, 0f * power, 0f)
-        val object2: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", 0f, -5f, 3f, -3f, 2f, -1f, 0f)
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "translationX", 0f * power, -25f * power, 20f * power, -15f * power, 10f * power, -5f * power, 0f * power, 0f)
+        val o2: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", 0f, -5f, 3f, -3f, 2f, -1f, 0f)
 
-        return AnimSet.set(view, duration, object1, object2)
+        return AnimSet.set(view, duration, o1, o2)
     }
 
     fun strength(view: View, duration: Long = 1000): AnimatorSet {
         val width = (view.width).toFloat()
         val power = (width / 100.0).toFloat()
 
-        val object1: ObjectAnimator = ObjectAnimator.ofFloat(view, "translationX", 0f * power, -25f * power, 20f * power, -15f * power, 10f * power, -5f * power, 0f * power, 0f)
-        val object2: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", 0f, -5f, 3f, -3f, 2f, -1f, 0f)
+        val o1: ObjectAnimator = ObjectAnimator.ofFloat(view, "translationX", 0f * power, -25f * power, 20f * power, -15f * power, 10f * power, -5f * power, 0f * power, 0f)
+        val o2: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", 0f, -5f, 3f, -3f, 2f, -1f, 0f)
 
-        return AnimSet.set(view, duration, object1, object2)
+        return AnimSet.set(view, duration, o1, o2)
     }
 }
