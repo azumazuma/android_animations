11fd4297dfc1d2ef252c27134d95fa8ce5e07016
Ryuse Azuma <azuma.ryusei@ditgroup.jp>
2019-11-18 12:52:14 +0900

    anim add

---
+++ androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Door.kt
@@ -0,0 +1,38 @@
+package com.azuma.androidcustomview.animation
+
+import android.animation.AnimatorSet
+import android.view.View
+
+class Door {
+    fun topOpen(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+
+    fun topClose(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+
+    fun bottomOpen(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+
+    fun bottomClose(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+
+    fun leftOpen(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+
+    fun leftClose(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+
+    fun rightOpen(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+
+    fun rightClose(view: View, duration: Long = 1000): AnimatorSet {
+        return AnimatorSet()
+    }
+}
