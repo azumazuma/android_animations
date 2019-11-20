bfe82cae0070dcb352e215d49ce36b0e014cbea6
Ryuse Azuma <azuma.ryusei@ditgroup.jp>
2019-11-20 12:58:11 +0900

    add anim

--- androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Roll.kt
+++ androidcustomview/src/main/java/com/azuma/androidcustomview/animation/Roll.kt
@@ -1,7 +1,9 @@
 package com.azuma.androidcustomview.animation
 
 import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.view.View
+import com.azuma.androidcustomview.util.AnimSet
 
 class Roll {
     fun topInSide(view: View, duration: Long = 1000): AnimatorSet {
@@ -20,13 +22,16 @@ class Roll {
     }
 
     fun bottomOutSide(view: View, duration: Long = 1000): AnimatorSet {
-
+        
         return AnimatorSet()
     }
 
     fun leftInSide(view: View, duration: Long = 1000): AnimatorSet {
+        val start = -view.right.toFloat()
+        val o1 = ObjectAnimator.ofFloat(view, "translationX", start, 0f)
+        val o2: ObjectAnimator = ObjectAnimator.ofFloat(view, "rotation", -180f, 0f)
 
-        return AnimatorSet()
+        return AnimSet.set(view, duration, o1, o2)
     }
 
     fun leftOutSide(view: View, duration: Long = 1000): AnimatorSet {
